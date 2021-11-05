3124bcaf05a312ae44099dd8b9b87b130f2f2ab6
noearaujo <araujonoe21@hotmail.com>
2021-11-05 19:06:42 -0300

    nova aparencia

--- src/App.js
+++ src/App.js
@@ -5,18 +5,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <main className="container-fluid mt-5">
+          <div className = "row">
+            <div className = "mb-4 mt-5 justify-content-center">
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+          </div>
+
+          <div className = "justify-content-center">
+            <button className= "menu-item" onClick = {() => alert("Em breve")}>
+              GAMES
+            </button>
+            <button className= "p-2 menu-item" onClick = {() => alert("Em breve")}>
+              APPS
+            </button>
+            <button className= "p-2 menu-item" onClick = {() => alert("Em breve")}>
+              COMPANY
+            </button>
+            <button className= "p-2 menu-item" onClick = {() => alert("Em breve")}>
+              CONTACT
+            </button>
+          </div>
+        </main>
+
+        <footer class="fixed-bottom text-center text-lg-center">
+		      <div>&copy; {new Date().getFullYear()} R13B. All rights reserved.</div>
+	      </footer>
       </header>
     </div>
   );
